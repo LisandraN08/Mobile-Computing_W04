4ac9a73c79509e35770387d00067a8df32f93dc3
MacBook Pro <mac@cip-008-ada.local>
2023-10-06 08:05:27 +0700

    init commit

---
+++ Mobile Computing_W04/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mobile Computing_W04
+//
+//  Created by MacBook Pro on 06/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        Text("Halo")
+        Text("coba Spacer")
+        Spacer()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mobile Computing_W04/Mobile_Computing_W04App.swift
@@ -0,0 +1,17 @@
+//
+//  Mobile_Computing_W04App.swift
+//  Mobile Computing_W04
+//
+//  Created by MacBook Pro on 06/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Mobile_Computing_W04App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
