e67ac453ec6dba3b9ed1e02ff766c44bb1e62776
MacBook Pro <mac@cip-008-ada.local>
2023-10-06 10:06:35 +0700

    pelajaran hr ini

--- Mobile Computing_W04/ContentView.swift
+++ Mobile Computing_W04/ContentView.swift
@@ -8,11 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var jumlahTekan = 0
+    @State var namaKu = ""
+    let daftarJagoanKu = ["Gwen", "Zoya", "Vanessa"]
+    
+    @State private var pilihanJagoanKu = "Zoya"
+    
     var body: some View {
-        
-        Text("Halo")
-        Text("coba Spacer")
-        Spacer()
+        NavigationView {
+            Form {
+                Section{
+                    Text("Halo")
+                    Text("Apa kabar")
+                    Button("Jumlah Tekan : \(jumlahTekan)"){
+                        print("Tertekan")
+                        jumlahTekan += 1
+                        print(namaKu)
+                    }
+                    TextField("Namanya siapa :", text: $namaKu)
+                    Text("Nama yang diinput adalah : \(namaKu)")
+                    Picker("Pilih jagoanmu", selection:$pilihanJagoanKu){
+                        ForEach(daftarJagoanKu, id: \.self){
+                            Text($0)
+                        }
+                    }.pickerStyle(.segmented)
+                }
+            }
+            .navigationTitle("Ini judul")
+            .navigationBarTitleDisplayMode(.large)
+        }
     }
 }
 
